2bf3e4ad5cfb710b6279d7e017320808f9e132ac
Jessica Sachs <jess@jessicasachs.io>
2020-12-07 01:14:07 -0500

    Adding new packages and jest config to lesson 2 - ending

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
-};
+  globals: {},
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\js$": "babel-jest"
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
+}
